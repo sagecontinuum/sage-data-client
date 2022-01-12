dfd4ccad87a2d907b76d63f1a42d1aed7ab77dd1
Sean Shahkarami <sean.shahkarami@gmail.com>
2022-01-12 12:55:49 -0600

    streamlined temperature example

--- examples/temperature_stats.py
+++ examples/temperature_stats.py
@@ -11,11 +11,5 @@ df = sage_data_client.query(
     }
 )
 
-# print results in data frame
-print(df)
-
-# meta columns are expanded into meta.fieldname. for example, here we print the unique nodes
-print(df["meta.vsn"].unique())
-
 # print stats of the temperature data grouped by node + sensor.
 print(df.groupby(["meta.vsn", "meta.sensor"]).value.agg(["size", "min", "max", "mean"]))
