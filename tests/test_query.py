2f5c1c59cb714545b8a2cb436e9f6c402c61f34f
Sean Shahkarami <sean.shahkarami@gmail.com>
2023-09-28 13:10:58 -0500

    falling back to json module instead of pyarrow due to not supporting mixed data types

--- tests/test_query.py
+++ tests/test_query.py
@@ -188,6 +188,16 @@ class TestQuery(unittest.TestCase):
         # NOTE In Pandas Nones and NaNs do not equal themselves, so will fill them to make df1 == df2 work.
         self.assertTrue((df1.fillna("") == df2.fillna("")).all().all())
 
+    def test_mixed_types(self):
+        sample_data = BytesIO(
+            b"""{"timestamp":"2021-10-14T21:42:21.149425156Z","name":"test","value":21.74,"meta":{"host":"0000dca632a3069f.ws-rpi","job":"sage","node":"000048b02d15c31f","plugin":"plugin-iio:0.4.5","sensor":"bme680","task":"iio-rpi","vsn":"W01C"}}
+{"timestamp":"2021-10-14T21:42:09.201150729Z","name":"test","value":"26.09","meta":{"host":"0000dca632a306d8.ws-rpi","job":"sage","node":"000048b02d15c31a","plugin":"plugin-iio:0.4.5","sensor":"bme680","task":"iio-shield","vsn":"W01A"}}
+"""
+        )
+        df = sage_data_client.load(sample_data)
+        self.assertEqual(len(df), 2)
+        self.assertValueResponse(df)
+
 
 if __name__ == "__main__":
     unittest.main()
