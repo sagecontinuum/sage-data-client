75fc776824d04bb4a70e1c30ff7e3e7713312886
Sean Shahkarami <sean.shahkarami@gmail.com>
2023-11-03 09:06:29 -0500

    expanded on docstring

--- examples/trigger-batch.py
+++ examples/trigger-batch.py
@@ -1,6 +1,11 @@
 """
-This example shows how to implement a simple batch trigger and prints all temperature
-events above a threshold.
+This is an example of a simple edge-to-cloud batch trigger which uses sage-data-client
+to gather and aggregate internal temperature data every 5 minutes and prints all
+nodes which exceed a threshold.
+
+Although it's simple, this example could easily be extended in multiple ways. For example:
+* Instead of just printing, an alert to be posted to Slack.
+* Instead of a fixed threshold, the typical value across all nodes could be used to determine outliers.
 """
 import sage_data_client
 import time
@@ -12,6 +17,8 @@ def main():
         "sensor": "bme280",
     }
 
+    threshold = 55.0
+
     while True:
         # get the last 5m of temperature data
         df = sage_data_client.query(start="-5m", filter=filter)
@@ -20,7 +27,7 @@ def main():
         mean_temps = df.groupby("meta.vsn").value.mean()
 
         # print values which exceed threshold
-        print(mean_temps[mean_temps > 55.0])
+        print(mean_temps[mean_temps > threshold])
 
         # wait 5m
         time.sleep(300)
